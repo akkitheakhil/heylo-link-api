1035974d9d5a2c211291714aa91c5d138aa9288d
akkitheakhil <akhilpad007@gmail.com>
2020-10-24 22:28:11 +0530

    Pushing UI to master

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan('common'));
 app.use(express.json());
-
+app.use(express.static('./public'));
 
 // End Points
-app.get('/', (req, res, next) => {
+app.get('/:id', (req, res, next) => {
     try {
 
         res.json({
